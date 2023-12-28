8bb9ac63512ae8145c1a33495344f16ae05f02f2
mimiron <mimirondakota@gmail.com>
2023-12-28 12:51:46 +0300

    Fisrt commit

---
+++ main.py
@@ -0,0 +1,119 @@
+
+
+# d = {}
+# d = dict()
+# d = {d:'a'}
+
+# word = "Lorem Ipsum Dolor Sit Amet"
+# print(word.count('L'))
+
+#numbers = [5, 5, 3, 4, 5, 2, 3, 5, 4]
+#number = [*numbers]
+#print(*number)
+#print(*numbers)
+
+
+# a, b = 2, 5
+# a, b = b, a
+# print(a, b)
+
+# #Задаем список
+# listNumbers = ['1', '1', '4', '5', '6', '4', '2', '3', '7', '5', '4', '2']
+# #Преобразуес список во множество
+# setNumbers = set(listNumbers)
+# for setNumber in setNumbers:
+#     print(setNumber)
+
+#numbers = ["Tom", "Jerry", 1, 2, 3]
+#print(numbers)
+
+# numbers = [1,2,3,4,5,6,7,8,9,0]
+# numbers2 = [11, 12, 13]
+# d = 15
+# s = 'Это список'
+# print(numbers)
+# print(numbers[2:7])
+# print(sum(numbers))
+# numbers.append(d)
+# print(numbers)
+# numbers.append(s)
+# print(numbers)
+# numbers = numbers + numbers2
+# print(numbers)
+
+#word = 'Lorem Ipsum Dolor Sit Amet'
+#print(word.lower())
+#print(word.upper())
+#print(word.replace(' ',','))
+
+# word = 'spamspamspam'
+# print(word[2:6:1])
+# print(word[1:10:4])
+# print(word[::-1])
+
+# word1 = "Python"
+# word2 = "Git"
+# print(word1 + " " + word2)
+# print(word1 * 2, word2 * 3)
+# print(len((word1 + " " + word2)))
+
+# houseroom = 45
+# lst = 20
+# print(f"номер подъезда:{(houseroom // lst) - 1}" )
+
+# lst = 100
+# lst2 = -3
+#
+# print(f"Сложение: {lst + lst2}, Вычитание: {lst - lst2}, Умножение: {lst * lst2}, Деление: {lst / lst2}, Возведение в степень: {lst ** lst2}")
+# print(f"Остаток от деления: {lst % lst2}, Получение целой части: {lst // lst2}, Замена знака: {-lst, -lst2}")
+
+# first_name = 'Jack'
+# last_name = 'Sparrow'
+# print(f"Ваше имя: {first_name}, ваша фамилия: {last_name}")
+
+# lst = ['hello', 'world']
+# for n, line in enumerate(lst, 1):
+#     if len(lst) == n:
+#          print(line)
+#     else:
+#          print(line, end=' ')
+
+#print(' Hello ',' world ', sep='|')
+
+#print(" hello world!")
+
+
+
+
+
+
+
+
+
+# lst = 'spam'
+# print('spam' * 3)
+
+# s1 = 'spam'
+# s2 = 'eggs'
+# print(s1 + s2)
+
+#day = 6
+#while day != 365:
+#   day += 1
+#    print(f"До нового года осталось {365 - day} дней")
+
+
+# tuple1 = (1, 2, 7, 6, 0, 3, 0, 0, 7, 4, 97, 0)
+# s = set(tuple1)
+#
+# print(s)
+
+#
+# lst = (1, 2, 7, 6, 0, 3)
+# lst2 = list()
+# for i in lst:
+#         if i % 2 == 1:
+#             lst2 += [i]
+#
+# print(type(lst))
+# print(lst2)
